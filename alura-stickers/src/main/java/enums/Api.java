c1463c041976f7a1516dae70a7332d893b71e131
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-07-22 16:25:24 -0300

    add link heroku

--- alura-stickers/src/main/java/enums/Api.java
+++ alura-stickers/src/main/java/enums/Api.java
@@ -6,7 +6,7 @@ public enum Api {
     IMDB_MAIOR("https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060"),
     NASA("https://api.nasa.gov/planetary/apod?api_key=v4lsy5rPlChkGbjV9kCLVokIJE3Fr6Y99rYrPD5A&start_date=2022-06-10&end_date=2022-06-14"),
     NASA_MOCK("https://api.mocki.io/v2/549a5d8b/NASA-APOD"),
-    LINGUAGENS("http://localhost:8080/linguagens");
+    LINGUAGENS("https://rcarteri-linguagens-api.herokuapp.com/linguagens");
 
     private final String url;
 
